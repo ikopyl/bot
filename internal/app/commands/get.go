24ac088a30e36447f1f6bf606835fd50e5a4ffeb
Ilya Kopyl <ilya.kopyl@salesforce.com>
2022-07-06 21:26:50 -0700

    Added a new command 'get', an initial version.

---
+++ internal/app/commands/get.go
@@ -0,0 +1,14 @@
+package commands
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func (c *Commander) Get(inputMsg *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMsg.Chat.ID, "TBD")
+	c.bot.Send(msg)
+}
+
+func init() {
+	registeredCommands["get"] = (*Commander).Get
+}
