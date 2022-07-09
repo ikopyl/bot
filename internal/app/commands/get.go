951e568bbe295f593326bbb3f3bdffde1adf8821
Ilya Kopyl <ilya.kopyl@salesforce.com>
2022-07-09 13:38:36 -0700

    Modified get command to parse an argument

--- internal/app/commands/get.go
+++ internal/app/commands/get.go
@@ -1,10 +1,25 @@
 package commands
 
 import (
+	"fmt"
+	"log"
+	"strconv"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func (c *Commander) Get(inputMsg *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMsg.Chat.ID, "TBD")
+	args := inputMsg.CommandArguments()
+
+	arg, err := strconv.Atoi(args)
+	if err != nil {
+		log.Println("wrong arg", args)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(
+		inputMsg.Chat.ID,
+		fmt.Sprintf("successfully parsed argument: %v", arg),
+	)
 	c.bot.Send(msg)
 }
